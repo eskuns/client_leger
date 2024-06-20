7585ed1c79247d989247c4fa07729c40326860d5
Enzo Marques <enzomarques94500@gmail.com>
2024-06-20 23:59:39 +0200

    Mise à jour

--- lolfr/php/cookie.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
-    echo "<script>console.log('Debug Objects: " . $_SESSION['loggedin'] . "' );</script>";
-} else {
-    header("Location: http://localhost/lolbackup/index.php");
-}
-?>
